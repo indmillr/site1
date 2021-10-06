32c02b8549a96368a57c77cbd14aeffbe9d703eb
Ian Miller <theiandmiller@gmail.com>
2021-10-05 21:19:46 -0500

    updated cart.js to use local storage in order to maintain accurate count of shopping cart contents even if browser window is closed and reopened.

--- cart.js
+++ cart.js
@@ -1,7 +1,58 @@
 let carts = document.querySelectorAll('.add-cart');
 
-for (let i=0; i < carts.length; i++) {
+let products = [
+    {
+        name: 'Grey Tshirt',
+        tag: 'greytshirt',
+        price: 15,
+        inCart: 0
+    },
+    {
+        name: 'Grey Hoodie',
+        tag: 'greyhoodie',
+        price: 20,
+        inCart: 0
+    },
+    {
+        name: 'Black Tshirt',
+        tag: 'blacktshirt',
+        price: 10,
+        inCart: 0
+    },
+    {
+        name: 'Black Hoodie',
+        tag: 'blackhoodie',
+        price: 25,
+        inCart: 0
+    }
+]
+
+for (let i=0; i<carts.length; i++) {
     carts[i].addEventListener('click', () => {
-        console.log("added to cart");
+        cartNumbers();
     })
 }
+
+function onLoadCartNumbers () {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers) {
+        document.querySelector('.cart span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers() {
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+   
+    if( productNumbers ) {
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.cart span').textContent = productNumbers +1;
+    } else {
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.cart span').textContent = 1;
+    }
+}
+
+onLoadCartNumbers();
