4da5201f53d866c2e1daf53d34c40e8923bcf09d
Ian Miller <theiandmiller@gmail.com>
2021-10-06 21:29:46 -0500

    updated cart.js to import layout into cart.html based on cart contents

--- cart.js
+++ cart.js
@@ -81,7 +81,6 @@ function setItems(product) {
 }
 
 function totalCost(product) {
-    // console.log("The product price is", product.price);
     let cartCost = localStorage.getItem('totalCost');
     console.log("My cart cost is", cartCost);
     console.log(typeof cartCost );
@@ -97,4 +96,46 @@ function totalCost(product) {
 
 }
 
+
+function displayCart() {
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem('totalCost');
+
+   
+    if( cartItems && productContainer ) {
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <ion-icon name="close-circle"></ion-icon>
+                <img src="./images/${item.tag}.jpg">
+                <span>${item.name}</span>
+            </div>
+            <div class="price">$${item.price}.00</div>
+            <div class="quantity">
+                <ion-icon class="decrease " name="arrow-dropleft-circle"></ion-icon>
+                <span>${item.inCart}</span>
+                <ion-icon class="increase " name="arrow-dropright-circle"></ion-icon>
+            </div>
+            <div class="total">
+                $${item.inCart * item.price}.00
+            </div>
+            `
+        });
+
+        productContainer.innerHTML += `
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">
+                    Total: 
+                </h4>
+                <h4 class="basketTotal">
+                    $${cartCost}.00
+                </h4>
+        `;
+    }
+}
+
 onLoadCartNumbers();
+displayCart();
